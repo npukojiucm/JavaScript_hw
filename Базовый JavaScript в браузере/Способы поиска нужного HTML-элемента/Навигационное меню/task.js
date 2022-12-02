0a64573a36b775c7a9f274a9533c7157dbc67e71
Alexandr <a.platcev@ya.ru>
2022-12-03 00:54:02 +0300

    error correction

--- Базовый JavaScript в браузере/Способы поиска нужного HTML-элемента/Навигационное меню/task.js
+++ Базовый JavaScript в браузере/Способы поиска нужного HTML-элемента/Навигационное меню/task.js
@@ -3,34 +3,42 @@
         return Array.from( document.getElementsByClassName( className ) ).filter(
             item => item.closest( `.${parentClass}`));
     }    
-    
-    const menuMain = getFilterArrayElements("menu__link", "menu_main"),
-        menuFoot = getFilterArrayElements("menu__link", "menu_foot");
-
-    menuMain.forEach(item => item.onclick = () => {
-        const menuSubMain = getFilterArrayElements("menu_active", "menu_main");
         
-        if (menuSubMain.length > 0) {
-            menuSubMain.forEach(item => item.classList.remove( "menu_active" ));
-        }
+    const menuMain = getFilterArrayElements("menu__link", "menu_main").filter(
+        item => item.nextElementSibling);
+    
+    const menuFoot = getFilterArrayElements("menu__link", "menu_foot").filter(
+        item => item.nextElementSibling);
 
-        const nextElement = item.nextElementSibling;  
-        if ( Boolean(nextElement) ) {
-            nextElement.classList.toggle( "menu_active" );
+    
+    menuMain.forEach((item, index) => item.onclick = () => {
+        const menuSubMain = getFilterArrayElements("menu_sub", "menu_main");
+        const indexActiveElem = menuSubMain.findIndex(item => item.classList.contains("menu_active"));
+
+        
+        if (indexActiveElem === -1 || indexActiveElem === index) {
+            menuSubMain[index].classList.toggle("menu_active");
+            return false;
+        
+        } else {
+            menuSubMain[indexActiveElem].classList.remove("menu_active");
+            menuSubMain[index].classList.toggle("menu_active");
             return false;
         }
-    })
-
-    menuFoot.forEach(item => item.onclick = () => {
-        const menuSubMain = getFilterArrayElements("menu_active", "menu_foot");
         
-        if (menuSubMain.length > 0) {
-            menuSubMain.forEach(item => item.classList.remove( "menu_active" ));
-        }
-
-        const nextElement = item.nextElementSibling;  
-        if ( Boolean(nextElement) ) {
-            nextElement.classList.toggle( "menu_active" );
+    });
+    
+    menuFoot.forEach((item, index) => item.onclick = () => {
+        const menuSubMain = getFilterArrayElements("menu_sub", "menu_foot");
+        const indexActiveElem = menuSubMain.findIndex(item => item.classList.contains("menu_active"));
+        
+        if (indexActiveElem === -1 || indexActiveElem === index) {
+            menuSubMain[index].classList.toggle("menu_active");
+            return false;
+        
+        } else {
+            menuSubMain[indexActiveElem].classList.remove("menu_active");
+            menuSubMain[index].classList.toggle("menu_active");
             return false;
         }
     })
--- Базовый JavaScript в браузере/Способы поиска нужного HTML-элемента/Слайдер/task.js
+++ Базовый JavaScript в браузере/Способы поиска нужного HTML-элемента/Слайдер/task.js
@@ -3,17 +3,15 @@
         return Array.from( document.getElementsByClassName( className ) );
     }
        
-    function removeClassNameAndReturnIndex(arraySelector, removeSelector) {
-        for (let i = 0; i < arraySelector.length; i++) {
-            if (arraySelector[i].classList.contains( removeSelector )) {
-                arraySelector[i].classList.toggle( removeSelector );
-                return i;
-            }
-        }
+    function removeClassAndReturnIndex(arrClass, removeClass) {
+        const index = arrClass.findIndex(item => item.classList.contains(removeClass))
+        arrClass[index].classList.remove(removeClass);
+        
+        return index;
     }
     
     function addSelector(array, index, className) {
-        array[ index ].classList.add( className)
+        return array[index].classList.add(className);
     }
      
     
@@ -22,33 +20,34 @@
         const sliderItemElements = getArrayElementsByClassName( "slider__item" ),
             sliderDotElements = getArrayElementsByClassName( "slider__dot" );
         
-        let removeIndexItem = removeClassNameAndReturnIndex(sliderItemElements, "slider__item_active"),
-            removeIndexDot = removeClassNameAndReturnIndex(sliderDotElements, "slider__dot_active");
+        let indexItem = removeClassAndReturnIndex(sliderItemElements, "slider__item_active");
+        
+        removeClassAndReturnIndex(sliderDotElements, "slider__dot_active");
         
         if (item.classList.contains( "slider__arrow_prev" )) {
-            if ( removeIndexItem === 0 ) {
-                removeIndexItem = sliderItemElements.length - 1;
+            if ( indexItem === 0 ) {
+                indexItem = sliderItemElements.length - 1;
             } else {
-                removeIndexItem -= 1;
+                indexItem -= 1;
             }
             
         } else if (item.classList.contains( "slider__arrow_next" )){
-            if ( removeIndexItem === sliderItemElements.length - 1 ) {
-                removeIndexItem = 0;
+            if ( indexItem === sliderItemElements.length - 1 ) {
+                indexItem = 0;
             } else {
-                removeIndexItem += 1;
+                indexItem += 1;
             }
         }
-        addSelector(sliderItemElements, removeIndexItem, "slider__item_active");
-        addSelector(sliderDotElements, removeIndexItem, "slider__dot_active");
+        addSelector(sliderItemElements, indexItem, "slider__item_active");
+        addSelector(sliderDotElements, indexItem, "slider__dot_active");
     })
     
     getArrayElementsByClassName( "slider__dot" ).forEach((item, index) => item.onclick = () => {
         const sliderItemElements = getArrayElementsByClassName( "slider__item" ),
             sliderDotElements = getArrayElementsByClassName( "slider__dot" );
         
-        let removeIndexItem = removeClassNameAndReturnIndex(sliderItemElements, "slider__item_active"),
-            removeIndexDot = removeClassNameAndReturnIndex(sliderDotElements, "slider__dot_active");
+        removeClassAndReturnIndex(sliderItemElements, "slider__item_active");
+        removeClassAndReturnIndex(sliderDotElements, "slider__dot_active");
         
         addSelector(sliderItemElements, index, "slider__item_active");
         addSelector(sliderDotElements, index, "slider__dot_active");
